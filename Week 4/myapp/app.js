aa2450919691233f160c4ff316d6349b3bb50be8
aghebert <30908336+aghebert@users.noreply.github.com>
2019-02-05 17:38:58 -0500

    Commit 2

---
+++ Week 4/myapp/app.js
@@ -0,0 +1,58 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const hbs = require('hbs');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+num = 4;
+var color = ((1<<24)*Math.random()|0).toString(16);
+
+hbs.registerHelper('generateTable',(num)=>{
+  
+  var msg = '';
+  for(let i=0; i<num; i++)
+  {
+      msg+=`<td style="background-color:#${colorRGB};">${colorRGB}<br /> <span syle="color:#ffffff;">${colorRGB}</span>span</td>`;
+  }
+
+  return new hbs.handlebars.SafeString(msg);
+});
+
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
