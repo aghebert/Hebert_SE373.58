182ad8665455240db0e5a29c0a67522db805db4b
aghebert <30908336+aghebert@users.noreply.github.com>
2019-03-14 17:56:24 -0400

    Get and Input are working

--- Week 6/week 6 assignment/myapp/routes/index.js
+++ Week 6/week 6 assignment/myapp/routes/index.js
@@ -6,85 +6,112 @@ var hbs = require('hbs');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-      res.render('index', {
-        title: 'Express'})
-        
-      });
-
-      /* GET home page. */
-      router.get('/create', function (req, res, next) {
-        res.render('index', {
-          title: 'Express'
-        });
-        var firstName = req.query.firstName;
-        var lastName = req.query.lastName;
-        var department = req.query.department;
-        var startDate = req.query.startDate;
-        var jobTitle = req.query.jobTitle;
-        var salary = req.query.salary;
-
-        create(firstName, lastName, department, startDate, jobTitle, salary);
-
-      });
-
-      mongoose.connect('mongodb://localhost/MongooseCRUD', {
-        useNewUrlParser: true
-      });
-
-
-
-      // grab the things we need
-      var mongoose = require('mongoose');
-      var Schema = mongoose.Schema;
-
-      // create a schema
-      var employeeSchema = new Schema({
-        firstName: String,
-        lastName: String,
-        department: String,
-        startDate: {
-          type: Date,
-          default: Date.now
-        },
-        jobTitle: String,
-        salary: Number
-      });
-
-
-
-      var Employee = mongoose.model('Employee', employeeSchema);
-
-      function create(vfirstName, vlastName, vdepartment, vstartDate, vjobTitle, vsalary) {
-        var newEmployee = Employee({
-          firstName: vfirstName,
-          lastName: vlastName,
-          department: vdepartment,
-          startDate: vstartDate,
-          jobTitle: vjobTitle,
-          salary: vsalary
-        });
-
-        newEmployee.save(function (err) {
-          if (err) throw err;
-
-          console.log('User created!');
-
-        });
-      }
-
-
-
-      hbs.registerHelper('empList', function () {
-        Employee.find({}, function (err, employees) {
-         
-          
-              for (var employee in employees) {
-                console.log(employees[0].get("firstName"))
-              }
-              
-        })
-
+  res.render('index', {
+    title: 'Express'
+  })
+      var html = "<div>"
+      /*
+      var employees = Employee.find({}, function (err, employees) {
+        return employees;
       })
+      */
+      Employee.find(function (err, employees) {
+        //return JSON.stringify(employees);
+      
+
+      console.log(employees)
+      
+        for (var employee in employees) {
+          html += employees[employee].get("firstName");
+          html += "<br/>";
+          html += employees[employee].get("lastName");
+          html += "<br/>";
+          html += employees[employee].get("department");
+          html += "<br/>";
+          html += employees[employee].get("startDate");
+          html += "<br/>";
+          html += employees[employee].get("jobTitle");
+          html += "<br/>";
+          html += employees[employee].get("salary");
+          html += "<br/><br/>";
+          
+        }
+      html += "</div>";
+      //res.write(html);
+    });
+
+    hbs.registerHelper('empList', function () {
+      return html;
+    })
+  });
 
 
-      module.exports = router;
+    /* GET home page. */
+    router.get('/create', function (req, res, next) {
+      res.render('index', {
+        title: 'Express'
+      });
+      var firstName = req.query.firstName;
+      var lastName = req.query.lastName;
+      var department = req.query.department;
+      var startDate = req.query.startDate;
+      var jobTitle = req.query.jobTitle;
+      var salary = req.query.salary;
+
+      create(firstName, lastName, department, startDate, jobTitle, salary);
+
+    });
+
+    mongoose.connect('mongodb://localhost/MongooseCRUD', {
+      useNewUrlParser: true
+    });
+
+
+
+    // grab the things we need
+    var mongoose = require('mongoose');
+    var Schema = mongoose.Schema;
+
+    // create a schema
+    var employeeSchema = new Schema({
+      firstName: String,
+      lastName: String,
+      department: String,
+      startDate: {
+        type: Date,
+        default: Date.now
+      },
+      jobTitle: String,
+      salary: Number
+    });
+
+
+
+    var Employee = mongoose.model('Employee', employeeSchema);
+
+    function create(vfirstName, vlastName, vdepartment, vstartDate, vjobTitle, vsalary) {
+      var newEmployee = Employee({
+        firstName: vfirstName,
+        lastName: vlastName,
+        department: vdepartment,
+        startDate: vstartDate,
+        jobTitle: vjobTitle,
+        salary: vsalary
+      });
+
+      newEmployee.save(function (err) {
+        if (err) throw err;
+
+        console.log('User created!');
+
+      });
+    }
+
+
+    /*
+    hbs.registerHelper('empList', function () {
+      
+    })
+    */
+
+    module.exports = router;
